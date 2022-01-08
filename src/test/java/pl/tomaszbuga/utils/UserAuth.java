dbe27fdc0919167d6937bb5c556d4de4e331e66d
Tomasz Buga <kontakt@tomaszbuga.pl>
2022-01-08 18:22:54 +0100

    added token variable to UserAuth; it's more efficient to reuse the Token instead of multiplying API calls

--- src/test/java/pl/tomaszbuga/utils/UserAuth.java
+++ src/test/java/pl/tomaszbuga/utils/UserAuth.java
@@ -7,6 +7,7 @@ import static com.codeborne.selenide.Selenide.open;
 import static io.restassured.RestAssured.given;
 
 public class UserAuth {
+    private static String token = "";
 
     public static String getJwtToken() {
         String json = "{\"username\": \"tbuga\",\"password\": \"4dm1n?!\"}";
@@ -20,8 +21,27 @@ public class UserAuth {
     }
 
     public static void addAuthCookiesToDriver() {
+        generateTokenIfNeeded();
         open("http://localhost:4200");
-        Cookie cookie = new Cookie("token", getJwtToken());
+        Cookie cookie = new Cookie("token", getCurrentToken());
         WebDriverRunner.getWebDriver().manage().addCookie(cookie);
+    }
+
+    private static void generateTokenIfNeeded() {
+        // maybe an issue if the token expiration date is overdue
+        // in case of Test Suites that takes longer than 15-30 minutes per Run
+        // probable solution: make Token Validation API in backend and apply
+        // verification in tests if the Run takes longer than expiration date
+        if (token.equals("")) {
+            setCurrentToken();
+        }
+    }
+
+    private static String getCurrentToken() {
+        return token;
+    }
+
+    private static void setCurrentToken() {
+        token = getJwtToken();
     }
 }
